c76fed421e754068b4573db328a99c07e0e214e1
Fernando Sartori <fehsartorisos@gmail.com>
2023-06-14 18:55:55 -0300

    correção bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Home from './pages/home.tsx'
-import App from './App.tsx'
->>>>>>> 9952413a6420c8c68f6dcb80e3810a62b3e23317
 
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
-=======
-    <Home />
->>>>>>> 9952413a6420c8c68f6dcb80e3810a62b3e23317
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
