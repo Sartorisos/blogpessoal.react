56e40df99ac70ced43349e5feaeafc3574e19e43
Fernando Sartori <fehsartorisos@gmail.com>
2023-06-14 18:20:08 -0300

    home material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Home from './pages/home.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
+
